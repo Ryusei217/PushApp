cb14f42ebe526355d5bbc1b672ac3cf42ffbca63
Cristian Calderon <cristian@iqdevpro.com>
2016-11-22 15:56:22 -0600

    Inicializado repositorio

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-fcm.FCMPlugin",
+        "file": "plugins/cordova-plugin-fcm/www/FCMPlugin.js",
+        "pluginId": "cordova-plugin-fcm",
+        "clobbers": [
+            "FCMPlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-fcm": "1.1.5"
+};
+// BOTTOM OF METADATA
+});
